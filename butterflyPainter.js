335863a1a3b2231ff4e3c116de753e17ea4aceac
remdd <munkrat@gmail.com>
2017-11-13 14:58:26 +0000

    Refactor drawing controls to allow proper management of stroke undo history

--- butterflyPainter.js
+++ butterflyPainter.js
@@ -1,27 +1,164 @@
 $(document).ready(function(){
 
-	var brushPreview = $('#brushPreview');
-	var curColor = brushPreview.css('backgroundColor');
-	var clickX = [];
-	var clickY = [];
-	var clickDrag = [];
-	var clickColor = [];
-	var clickSize = [];
-	var curSize = brushPreview.width();
-	var paint = false;
-	var brushStrokes = []
+	var $brushPreview = $('#brushPreview');
+	var stroke = {};										//	Hold current stroke
+	var strokes = [];										//	Hold all strokes
+	var paint = false;										//	Set to 'true' when drawing a brush stroke
 	var eyedropperMode = false;
 	var eyedropperPicking = false;
-	context = document.getElementById('canvas').getContext("2d");
-	context.imageSmoothingEnabled = false;
-	context.lineJoin = "round";
 
-	$('#clearBtn').click(clearDown);
-	$('#undoBtn').click(undo);
-	$('#finishBtn').click(finish);
-	$('#typeEyedropperBtn').click(startEyeDropperMode);
-	$('#typeBrushBtn').click(endEyeDropperMode);
-	wingTemplate(8, true, true, true, false);
+	//	Define canvas context
+	var context = document.getElementById('canvas').getContext("2d");
+	context.imageSmoothingEnabled = false;
+	context.lineJoin = "round";			//	Brush shape
+	context.lineCap = "round";			//	Brush shape
+
+	//	Button click listeners
+	$('#clearBtn').click(reset);							//	Clear button
+	$('#undoBtn').click(undo);								//	Undo button
+	$('#finishBtn').click(finish);							//	Finish button
+	$('#typeEyedropperBtn').click(startEyeDropperMode);		//	Eyedropper mode button
+	$('#typeBrushBtn').click(endEyeDropperMode);			//	Brush mode button
+
+	wingTemplate(8, true, true);				//	Draw wing template
+
+	function addClick(x, y)	{
+		var click = { x: x, y: y }				//	Set latest click co-ordinates
+		stroke.clicks.push(click);				//	Pushes latest click to current stroke
+		drawStroke();							//	Updates canvas with latest click
+	}
+
+	function drawStroke() {
+		context.beginPath();								//	Start path
+		if(stroke.clicks.length > 1) {						//	Unless not the first click, set 'starting' co-ordinates to the previous click
+			context.moveTo(stroke.clicks[stroke.clicks.length - 2].x, stroke.clicks[stroke.clicks.length - 2].y);
+		}
+		context.lineTo(stroke.clicks[stroke.clicks.length - 1].x, stroke.clicks[stroke.clicks.length - 1].y);		//	Draw line to most recent click position
+		context.strokeStyle = stroke.color;					//	Set stroke colour
+		context.lineWidth = stroke.size;					//	Set brush size
+		context.closePath();								//	End path
+		context.stroke();									//	Draw stroke on canvas
+	}
+
+	function redrawStroke() {
+		context.beginPath();								//	Start path
+		context.lineTo(stroke.clicks[0].x, stroke.clicks[0].y);		//	Start at position of first click in stroke's array
+		for(var i = 0; i < stroke.clicks.length; i++) {				//	Iterate clicks in stroke's array
+			if(i > 0) {												//	Unless this is the first click in array, start from position of previous click
+				context.moveTo(stroke.clicks[i - 1].x, stroke.clicks[i - 1].y)
+			}
+			context.lineTo(stroke.clicks[i].x, stroke.clicks[i].y);	//	Draw line to current click position
+		}
+		context.strokeStyle = stroke.color;
+		context.lineWidth = stroke.size;
+		context.closePath();
+		context.stroke();		
+	}
+
+	function newStroke(x, y) {								//	Called when starting a new 'stroke' object
+		stroke = {
+			color: $brushPreview.css('backgroundColor'),	//	Set colour
+			size: $brushPreview.width(),					//	Set size
+			clicks: [{"x": x, "y": y}]						//	Set co-ordinates of first click
+		}
+		drawStroke();										//	Draw on canvas
+	}
+
+	//	Called on mousedown & touchstart events
+	function touchCanvas(x, y) {
+		if(eyedropperMode) {
+			eyedropperPicking = true;
+			getCanvasColor(x, y);
+		} else {
+			paint = true;
+			newStroke(x, y);
+		}
+	}
+
+	//	Called on mousemove & touchmove events
+	function dragCanvas(x, y) {
+		if(eyedropperMode && eyedropperPicking) {
+			getCanvasColor(x, y);
+		} else if(paint){
+			addClick(x, y);
+		}
+	}
+
+	// Mouse controls
+	$('#canvas').on("mousedown", function(e){
+		var x = e.pageX - this.offsetLeft;
+		var y = e.pageY - this.offsetTop;
+		touchCanvas(x, y);
+	});
+	$('#canvas').on("mousemove", function(e){
+		var x = e.pageX - this.offsetLeft;
+		var y = e.pageY - this.offsetTop;
+		dragCanvas(x, y);
+	});
+	// Touch controls
+	$('#canvas').on("touchstart", function(e){
+		var x = e.touches[0].clientX - this.offsetLeft;
+		var y = e.touches[0].clientY - this.offsetTop;
+		touchCanvas(x, y);
+	});
+	$('#canvas').on("touchmove", function(e){
+		var x = e.touches[0].clientX - this.offsetLeft;
+		var y = e.touches[0].clientY - this.offsetTop;
+		dragCanvas(x, y);
+	});
+	// Mouse & touch controls
+	$(document).on("mouseup touchend touchcancel", function(e){
+		paint = false;
+		eyedropperPicking = false;
+		endEyeDropperMode();
+		if(stroke.clicks && stroke.clicks.length > 0) {
+			strokes.push(stroke); 										//	'stroke' object is pushed to 'strokes' array
+			stroke = {};												//	'stroke' object is cleared ready for next newStroke
+			console.log(strokes.length);
+		}
+	});
+
+	//	Update brush preview colour (used by eyedropper mode as well as palettes)
+	function getCanvasColor(mouseX, mouseY) {
+		var touchData = context.getImageData(mouseX, mouseY, 1, 1);
+		var touchColor = 'rgba(' + touchData.data[0] + ',' + touchData.data[1] +',' + touchData.data[2] + ',' + touchData.data[3] + ')';
+		$brushPreview.css('backgroundColor', touchColor);
+	}
+
+	//	Clear canvas and redraw stored 'strokes' from array
+	function redraw() {
+		clearCanvas();
+		if(strokes.length > 0) {
+			for(var i = 0; i < strokes.length; i++) {
+				console.log("Drawing stroke " + i);
+				stroke = strokes[i];
+				redrawStroke();
+			}
+		}
+		stroke = {};
+	}
+
+	//	Pop last 'stroke' & redraw canvas
+	function undo() {
+		console.log("undo!");
+		strokes.pop();
+		redraw();
+	}
+
+	//	Clear and re-initialize canvas context
+	function clearCanvas() {
+		console.log("clearing down");
+		context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+		wingTemplate(8, true, true);
+	}
+	//	Clear all stored strokes & re-initialize canvas context
+	function reset() {
+		stroke = {};
+		strokes = [];
+		clearCanvas();
+	}
+
+	//	Solid code from here *******************************************************************************************************************
 
 	function startEyeDropperMode () {
 		$('#typeBrushBtn').removeClass('active');
@@ -38,156 +175,44 @@ $(document).ready(function(){
 		eyedropperMode = false;
 	}
 
-	// Mouse controls
-	$('#canvas').on("mousedown", function(e){
-		var mouseX = e.pageX - this.offsetLeft;
-		var mouseY = e.pageY - this.offsetTop;
-		if(eyedropperMode) {
-			eyedropperPicking = true;
-			getCanvasColor(mouseX, mouseY);
-		} else {
-			curColor = brushPreview.css('backgroundColor');
-			paint = true;
-			addClick(mouseX, mouseY);
-			redraw();
-		}
-	});
-	$('#canvas').on("mousemove", function(e){
-		var mouseX = e.pageX - this.offsetLeft;
-		var mouseY = e.pageY - this.offsetTop;
-		if(eyedropperMode && eyedropperPicking) {
-			getCanvasColor(mouseX, mouseY);
-		} else if(paint){
-			addClick(mouseX, mouseY, true);
-			redraw();
-		}
-	});
-	// Touch controls
-	$('#canvas').on("touchstart", function(e){
-		var mouseX = e.touches[0].clientX - this.offsetLeft;
-		var mouseY = e.touches[0].clientY - this.offsetTop;
-		if(eyedropperMode) {
-			eyedropperPicking = true;
-			getCanvasColor(mouseX, mouseY);
-		} else {
-			curColor = brushPreview.css('backgroundColor');
-			paint = true;
-			addClick(mouseX, mouseY);
-			redraw();
-		}
-	});
-	$('#canvas').on("touchmove", function(e){
-		var mouseX = e.touches[0].clientX - this.offsetLeft;
-		var mouseY = e.touches[0].clientY - this.offsetTop;
-		if(eyedropperMode && eyedropperPicking) {
-			getCanvasColor(mouseX, mouseY);
-		} else if(paint){
-			addClick(mouseX, mouseY, true);
-			redraw();
-		}
-	});
-	// Mouse & touch controls
-	$(document).on("mouseup touchend touchcancel", function(e){
-		paint = false;
-		eyedropperPicking = false;
-		endEyeDropperMode();
-	});
-
-	function getCanvasColor(mouseX, mouseY) {
-		var touchData = context.getImageData(mouseX, mouseY, 1, 1);
-		var touchColor = 'rgba(' + touchData.data[0] + ',' + touchData.data[1] +',' + touchData.data[2] + ',' + touchData.data[3] + ')';
-		brushPreview.css('backgroundColor', touchColor);
-	}
-
-	function addClick(x, y, dragging)
-	{
-		clickColor.push(curColor);
-		clickSize.push(curSize);
-		clickX.push(x);
-		clickY.push(y);
-		clickDrag.push(dragging);
-	}
-
-	function redraw() {
-		for(var i=0; i < clickX.length; i++) {		
-			context.beginPath();
-			if(clickDrag[i] && i){
-				context.moveTo(clickX[i-1], clickY[i-1]);
-			}else{
-				context.moveTo(clickX[i]-1, clickY[i]);
-			}
-			context.lineTo(clickX[i], clickY[i]);
-			context.closePath();
-			context.strokeStyle = clickColor[i];
-			context.lineWidth = clickSize[i];
-			context.stroke();
-		}
-	}
-
-	function undo() {
-		console.log("undo!");
-		clickDrag.pop();
-		clickX.pop();
-		clickY.pop();
-		clickSize.pop();
-		clickColor.pop();
-		redraw();
-	}
-
-
-	function clearDown() {
-		console.log("clearing down");
-		context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-		clickDrag = [];
-		clickX = [];
-		clickY = [];
-		clickSize = [];
-		clickColor = [];
-		wingTemplate(8, true, true, true, false);
-	}
-
-
-
-	//	Solid code from here *******************************************************************************************************************
-
+	//	Export current canvas context to file, clear down & reset for next user
 	function finish() {
 		console.log("finishing...");
-		wingTemplate(12, false, false, false, true);
+		wingTemplate(8, false, false);
 		canvas.toBlob(function(blob) {
 	  		saveAs(blob, "NewButterfly.png");
 	  	});
-		clearDown();
+		reset();
 	//	window.open(canvas.toDataURL("image/png"));
 	}
 
 	// Brush size slider
 	$('#brushSizeSlider').on('input', function(){
-		brushPreview.width(this.value);
-		brushPreview.height(this.value);
-		curSize = brushPreview.width();
+		$brushPreview.width(this.value);
+		$brushPreview.height(this.value);
 	});
 
-	// Palette squares
-	$('.paletteSquare').click(function(){
-		var color = $(this).css('backgroundColor');
-		brushPreview.css('backgroundColor', color);
-	});
-
-	// Color picker
+	// Initialize color picker
 	$('#colorPicker').show();
 	ColorPicker(
 		document.getElementById('slider'),
 		document.getElementById('picker'),
 
 		function(hex, hsv, rgb) {
-			console.log(hsv.h, hsv.s, hsv.v);         // [0-359], [0-1], [0-1]
-			console.log(rgb.r, rgb.g, rgb.b);         // [0-255], [0-255], [0-255]
-			brushPreview.css('backgroundColor', hex);
-			clickColor.push()
+//			console.log(hsv.h, hsv.s, hsv.v);         // [0-359], [0-1], [0-1]
+//			console.log(rgb.r, rgb.g, rgb.b);         // [0-255], [0-255], [0-255]
+			$brushPreview.css('backgroundColor', hex);
 	});
 
-	// Clip path to outline of wing
-	function wingTemplate(linewidth, fill, clip, first, final) {
+	//	Update brush preview colour on touchdrag along colour slider
+	$('#slider').on('touchmove', function(e){
+		var mouseX = e.touches[0].clientX;
+		var mouseY = e.touches[0].clientY;
+		getCanvasColor(mouseX, mouseY);
+	});
+
+	//	Define wing outline as border of canvas context & clip strokes within it
+	function wingTemplate(linewidth, fill, clip) {
 		context.beginPath();
 		context.lineWidth = linewidth;
 		context.strokeStyle = "black";
@@ -218,17 +243,9 @@ $(document).ready(function(){
 			context.fillStyle = "white";
 			context.fill();
 		}
-		if(first) {
-			context.save();
-		}
 		if(clip) {
 			context.clip();
 		}
-		if(final) {
-			context.restore();
-	//		context.stroke();
-		}
 	}
-
 
 });
